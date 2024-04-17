963bec25dd24215c7621d22d20229b26942c7c9c
Saif hassouni <106547346+SaifeddineCode@users.noreply.github.com>
2024-04-17 14:36:30 +0100

    update first section of dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,15 +10,18 @@ import Countries from '../components/dashboard/Countries'
 import Conversion from '../components/dashboard/Conversion'
 import Languages from '../components/dashboard/Languages'
 import FirstSection from '../components/FirstSection'
+import { useSelector } from 'react-redux'
 
 const Dashboard = ({token}) => {
 
+
+  const {currentUser} = useSelector(state => state.user)
 
   return (
   
     <div>
       {/* <h2 className='font-semibold text-2xl' > Hi, welcome back </h2> */}
-      <FirstSection text={"Hi, Welcome back"} token={token}  />
+      <FirstSection text={`Hi, Welcome back, ${currentUser.full_name}`} token={token}  />
       <div className='mt-10 flex max-sm:flex-col gap-4 sm:flex-wrap md:flex-nowrap justify-center ' >
         <Statics bgIcon={"bg-gradient-to-l from-blue-300"} bg="bg-blue-100" icon={sales} num={"714k"} statics={"Weekly Sales"} textColor={"text-[#3f548e]"} numColor={"text-[#061b64]"}   />
         <Statics bgIcon={"bg-gradient-to-l from-cyan-300"} bg="bg-cyan-100" icon={user} num={"1.35m"} statics={"New Users"} textColor={"text-[#4064a1]"} numColor={"text-[#04297a]"}   />

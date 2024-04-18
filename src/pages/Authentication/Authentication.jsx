f70f3866332fba96dcc8f4b620238601f0d69ecb
Saif hassouni <106547346+SaifeddineCode@users.noreply.github.com>
2024-04-18 16:07:52 +0100

    Update Authentication.jsx

--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -4,7 +4,7 @@ import Login from '../Login'
 
 const Authentication = ({setToken}) => {
 
-    const [currentSession,setCurrentSession] = useState("register")
+    const [currentSession,setCurrentSession] = useState("login")
 
   return (
     <div>
